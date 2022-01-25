784d3cc2f43a31a54b19c4f9b49997d858397b65
Natdanai Tinnakon <newsnail081@gmail.com>
2022-01-25 14:56:10 +0700

    first commit

---
+++ src/views/Tab1.vue
@@ -0,0 +1,45 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>หน้าหลัก</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+
+      <ion-grid>
+        <ion-row>
+          <ion-col>
+            <ion-button>button 1</ion-button>
+          </ion-col>
+          <ion-col>
+            <ion-button color="danger" size="small">button 2</ion-button>
+          </ion-col>
+        </ion-row>
+        <ion-row>
+          <ion-col>
+            <ion-button color="success" size="large">button 3</ion-button>
+          </ion-col>
+          <ion-col>
+            <ion-button shape="round" fill="outline">button 4</ion-button>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+      
+      
+      
+      
+      
+
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonGrid, IonRow, IonCol } from '@ionic/vue';
+
+export default  {
+  name: 'Tab1',
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonButton, IonGrid, IonRow, IonCol }
+}
+</script>
---
+++ src/views/Tab2.vue
@@ -0,0 +1,193 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>สินค้า</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-fab vertical="bottom" horizontal="end" slot="fixed">
+        <ion-fab-button @click="addProduct">
+          <ion-icon :icon="add"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
+
+      <ion-grid>
+        <ion-col v-for="data of dataProduct" :key="data.id">
+          <ion-card>
+            <ion-card-header>
+              <ion-card-title>{{ data.product_name }}</ion-card-title>
+            </ion-card-header>
+            <ion-card-content>
+              รหัสสินค้า {{ data.product_id }}
+            </ion-card-content>
+            <ion-card-content>
+              <ion-button size="small" @click="buy(data.product_id)"
+                >ราคา {{ data.product_price }}</ion-button
+              >
+            </ion-card-content>
+          </ion-card>
+        </ion-col>
+      </ion-grid>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import axios from "axios";
+
+import {
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  alertController,
+  IonButton,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonGrid,
+  IonCol,
+} from "@ionic/vue";
+
+import { add } from "ionicons/icons";
+
+export default {
+  name: "Tab2",
+  components: {
+    IonFab,
+    IonFabButton,
+    IonIcon,
+    IonButton,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+    IonGrid,
+    IonCol,
+  },
+  setup() {
+    return {
+      add,
+    };
+  },
+  data() {
+    return {
+      dataProduct: "",
+    };
+  },
+  async created() {
+    this.allProduct();
+  },
+  methods: {
+    async allProduct() {
+      try {
+        const res = await axios.get("http://localhost:3000/mongo/products");
+        this.dataProduct = res.data;
+        // console.log("allProduct")
+        // console.log(this.dataProduct);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async addProduct() {
+      const alert = await alertController.create({
+        header: "เพิ่มข้อมูลสินค้า",
+        inputs: [
+          {
+            placeholder: "กรอกรหัสสินค้า",
+            name: "product_id",
+          },
+          {
+            placeholder: "กรอกชื่อสินค้า",
+            name: "product_name",
+          },
+          {
+            placeholder: "กรอกราคาสินค้า",
+            name: "product_price",
+            type: "number",
+          },
+        ],
+        buttons: [
+          {
+            text: "ยืนยัน",
+            handler: async (data) => {
+              if (data.product_id && data.product_name && data.product_price) {
+                // console.log("product_id : " + data.product_id);
+                // console.log("product_name : " + data.product_name);
+                // console.log("product_price : " + data.product_price);
+
+                await axios
+                  .post("http://localhost:3000/mongo/products", data)
+                  .then(function (res) {
+                    console.log(res.data.message);
+                  });
+
+                  this.allProduct();
+              } else {
+                console.log("กรุณากรอกข้อมูลให้ครบถ้วน");
+                this.checkinput();
+              }
+            },
+          },
+          {
+            text: "ยกเลิก",
+            handler: () => {
+              console.log("ยกเลิก");
+            },
+          },
+        ],
+      });
+
+      await alert.present();
+    },
+    async buy(id) {
+      const alert = await alertController.create({
+        header: "ยืนยันการสั่งซื้อ",
+        message: `สินค้ารหัส : ${id}`,
+        buttons: [
+          {
+            text: "ยืนยัน",
+            handler: () => {
+              console.log("ยืนยัน " + id);
+            },
+          },
+          {
+            text: "ยกเลิก",
+            handler: () => {
+              console.log("ยกเลิก");
+            },
+          },
+        ],
+      });
+      await alert.present();
+    },
+    async checkinput() {
+      const alert = await alertController.create({
+        header: "คำเตือน",
+        message: "กรุณากรอกข้อมูลให้ครบถ้วน",
+        buttons: ["OK"],
+      });
+      await alert.present();
+    },
+    async success(text) {
+      const alert = await alertController.create({
+        header: "สำเร็จ",
+        message: text,
+        buttons: ["OK"],
+      });
+      await alert.present();
+    },
+  },
+};
+</script>

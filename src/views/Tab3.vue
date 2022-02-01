76defee368ce98d42b28282332ef46518392ad8a
Natdanai Tinnakon <newsnail081@gmail.com>
2022-02-01 11:21:38 +0700

    Week 11

--- src/views/Tab3.vue
+++ src/views/Tab3.vue
@@ -7,13 +7,26 @@
     </ion-header>
     <ion-content :fullscreen="true">
 
+      <ion-button @click="addCustomer" color="success" size="small">เพิ่มข้อมูลลูกค้า</ion-button>
+
+      <ion-list>
+        <ion-item>
+          <ion-label>ชื่อ - นามสกุล : </ion-label>
+          <ion-input v-model="name" autofocus="true"></ion-input>
+        </ion-item>
+        <ion-item>
+          <ion-label>เบอร์โทรศัพท์ : </ion-label>
+          <ion-input v-model="tel"></ion-input>
+        </ion-item>
+      </ion-list>
+
       <ion-list>
         <ion-item-sliding>
           <ion-item>
             <ion-avatar slot="start">
               <img src="../../public/assets/icon/icon.png" />
             </ion-avatar>
-            <ion-label>นายณัฐดนัย ทินกร </ion-label>
+            <ion-label>นายณัฐดนัย ทินกร</ion-label>
           </ion-item>
           <ion-item-options slot="end">
             <ion-item-option>เพิ่มเติม</ion-item-option>
@@ -24,7 +37,7 @@
             <ion-avatar slot="start">
               <img src="../../public/assets/icon/icon.png" />
             </ion-avatar>
-            <ion-label>นายขโมย ใจดำ</ion-label>
+            <ion-label>นายเทพ ดวงดี </ion-label>
           </ion-item>
           <ion-item-options slot="end">
             <ion-item-option>เพิ่มเติม</ion-item-option>
@@ -35,7 +48,7 @@
             <ion-avatar slot="start">
               <img src="../../public/assets/icon/icon.png" />
             </ion-avatar>
-            <ion-label>นางสาววันดี จันทร์วัน</ion-label>
+            <ion-label>นางสาวสมหญิง วันจันทร์</ion-label>
           </ion-item>
           <ion-item-options slot="end">
             <ion-item-option>เพิ่มเติม</ion-item-option>
@@ -43,7 +56,7 @@
         </ion-item-sliding>
       </ion-list>
 
-      <ion-list>
+      <!-- <ion-list>
         <ion-item>
           <ion-label>ชื่อ - นามสกุล : </ion-label>
           <ion-input></ion-input>
@@ -76,17 +89,69 @@
           <ion-label>Checkbox 2</ion-label>
           <ion-checkbox slot="end"></ion-checkbox>
         </ion-item>
-      </ion-list>
+      </ion-list> -->
 
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
-import { IonItemOption, IonItemOptions, IonItemSliding, IonAvatar, IonCheckbox, IonRadio, IonToggle, IonInput, IonList, IonItem, IonLabel, IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+<script>
+import { alertController, IonButton, IonItemOption, IonItemOptions, IonItemSliding, IonAvatar, IonInput, IonList, IonItem, IonLabel, IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import axios from 'axios';
 
 export default  {
   name: 'Tab3',
-  components: { IonItemOption, IonItemOptions, IonItemSliding, IonAvatar, IonCheckbox, IonRadio, IonToggle, IonInput, IonList, IonItem, IonLabel, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { IonButton ,IonItemOption, IonItemOptions, IonItemSliding, IonAvatar, IonInput, IonList, IonItem, IonLabel, IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
+  data(){
+    return {
+      name: "",
+      tel: ""
+    }
+  },
+  methods: {
+    clearInput(){
+      this.name = "";
+      this.tel = "";
+    },
+    async addCustomer(){
+
+      if(this.name.trim() !== "" && this.tel.trim() !== ""){
+        // console.log("เพิ่มข้อมูลลูกค้า");
+        // console.log(this.name.trim());
+        // console.log(this.tel.trim());
+        try {
+          const res = await axios.post("http://localhost:3000/mongo/customers", {
+            name: this.name.trim(),
+            tel: this.tel.trim()
+          });
+
+          this.clearInput();
+
+          const alert = await alertController.create({
+            header: "สำเร็จ",
+            message: res.data.message,
+            buttons: ['OK']
+          });
+
+          await alert.present();
+
+        } catch (error) {
+          console.log(error);
+        }
+
+      } else {
+
+        const alert = await alertController.create({
+          header: "คำเตือน",
+          message: "กรุณากรอกข้อมูลให้ครบถ้วน",
+          buttons: ['OK']
+        });
+
+        await alert.present();
+
+      }
+
+    }
+  }
 }
 </script>

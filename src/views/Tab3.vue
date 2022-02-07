8c978a241a9db29cc5a435bbde1ba9d063212975
Natdanai Tinnakon <newsnail081@gmail.com>
2022-02-07 20:22:05 +0700

    Week 11

--- src/views/Tab3.vue
+++ src/views/Tab3.vue
@@ -6,8 +6,9 @@
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-
-      <ion-button @click="addCustomer" color="success" size="small">เพิ่มข้อมูลลูกค้า</ion-button>
+      <ion-button @click="addCustomer" color="success" size="small"
+        >เพิ่มข้อมูลลูกค้า</ion-button
+      >
 
       <ion-list>
         <ion-item>
@@ -21,37 +22,15 @@
       </ion-list>
 
       <ion-list>
-        <ion-item-sliding>
+        <ion-item-sliding v-for="data of dataCustomer" :key="data.id">
           <ion-item>
             <ion-avatar slot="start">
               <img src="../../public/assets/icon/icon.png" />
             </ion-avatar>
-            <ion-label>นายณัฐดนัย ทินกร</ion-label>
+            <ion-label>{{ data.c_fullname }}</ion-label>
           </ion-item>
-          <ion-item-options slot="end">
-            <ion-item-option>เพิ่มเติม</ion-item-option>
-          </ion-item-options>
-        </ion-item-sliding>
-        <ion-item-sliding>
-          <ion-item>
-            <ion-avatar slot="start">
-              <img src="../../public/assets/icon/icon.png" />
-            </ion-avatar>
-            <ion-label>นายเทพ ดวงดี </ion-label>
-          </ion-item>
-          <ion-item-options slot="end">
-            <ion-item-option>เพิ่มเติม</ion-item-option>
-          </ion-item-options>
-        </ion-item-sliding>
-        <ion-item-sliding>
-          <ion-item>
-            <ion-avatar slot="start">
-              <img src="../../public/assets/icon/icon.png" />
-            </ion-avatar>
-            <ion-label>นางสาวสมหญิง วันจันทร์</ion-label>
-          </ion-item>
-          <ion-item-options slot="end">
-            <ion-item-option>เพิ่มเติม</ion-item-option>
+          <ion-item-options @click="detail(data.c_tel)" slot="end">
+            <ion-item-option>โทร</ion-item-option>
           </ion-item-options>
         </ion-item-sliding>
       </ion-list>
@@ -90,68 +69,117 @@
           <ion-checkbox slot="end"></ion-checkbox>
         </ion-item>
       </ion-list> -->
-
     </ion-content>
   </ion-page>
 </template>
 
 <script>
-import { alertController, IonButton, IonItemOption, IonItemOptions, IonItemSliding, IonAvatar, IonInput, IonList, IonItem, IonLabel, IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import axios from 'axios';
+import {
+  alertController,
+  IonButton,
+  IonItemOption,
+  IonItemOptions,
+  IonItemSliding,
+  IonAvatar,
+  IonInput,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from "@ionic/vue";
+import axios from "axios";
 
-export default  {
-  name: 'Tab3',
-  components: { IonButton ,IonItemOption, IonItemOptions, IonItemSliding, IonAvatar, IonInput, IonList, IonItem, IonLabel, IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
-  data(){
+export default {
+  name: "Tab3",
+  components: {
+    IonButton,
+    IonItemOption,
+    IonItemOptions,
+    IonItemSliding,
+    IonAvatar,
+    IonInput,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+  },
+  data() {
     return {
       name: "",
-      tel: ""
-    }
+      tel: "",
+      dataCustomer: "",
+    };
+  },
+  created() {
+    this.allCustomer();
   },
   methods: {
-    clearInput(){
+    async detail(tel) {
+      const alert = await alertController.create({
+        header: "เบอร์โทร",
+        message: tel,
+        buttons: ["OK"],
+      });
+
+      await alert.present();
+    },
+    async allCustomer() {
+      try {
+        const res = await axios.get("http://localhost:3000/mongo/customers");
+        this.dataCustomer = res.data;
+        // console.log(this.dataCustomer);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    clearInput() {
       this.name = "";
       this.tel = "";
     },
-    async addCustomer(){
-
-      if(this.name.trim() !== "" && this.tel.trim() !== ""){
+    async addCustomer() {
+      if (this.name.trim() !== "" && this.tel.trim() !== "") {
         // console.log("เพิ่มข้อมูลลูกค้า");
         // console.log(this.name.trim());
         // console.log(this.tel.trim());
         try {
-          const res = await axios.post("http://localhost:3000/mongo/customers", {
-            name: this.name.trim(),
-            tel: this.tel.trim()
-          });
+          const res = await axios.post(
+            "http://localhost:3000/mongo/customers",
+            {
+              name: this.name.trim(),
+              tel: this.tel.trim(),
+            } 
+          );
 
           this.clearInput();
 
           const alert = await alertController.create({
             header: "สำเร็จ",
             message: res.data.message,
-            buttons: ['OK']
+            buttons: ["OK"],
           });
 
           await alert.present();
-
+          this.allCustomer();
         } catch (error) {
           console.log(error);
         }
-
       } else {
-
         const alert = await alertController.create({
           header: "คำเตือน",
           message: "กรุณากรอกข้อมูลให้ครบถ้วน",
-          buttons: ['OK']
+          buttons: ["OK"],
         });
-
         await alert.present();
-
       }
-
-    }
-  }
-}
+    },
+  },
+};
 </script>

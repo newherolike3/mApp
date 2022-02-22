1a2d939ad02924f666e40c50ce4701706c56be3a
Natdanai Tinnakon <newsnail081@gmail.com>
2022-02-22 11:43:45 +0700

    Week13

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -1,45 +1,77 @@
 <template>
-  <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>หน้าหลัก</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content :fullscreen="true">
-
-      <ion-grid>
-        <ion-row>
-          <ion-col>
-            <ion-button>button 1</ion-button>
-          </ion-col>
-          <ion-col>
-            <ion-button color="danger" size="small">button 2</ion-button>
-          </ion-col>
-        </ion-row>
-        <ion-row>
-          <ion-col>
-            <ion-button color="success" size="large">button 3</ion-button>
-          </ion-col>
-          <ion-col>
-            <ion-button shape="round" fill="outline">button 4</ion-button>
-          </ion-col>
-        </ion-row>
-      </ion-grid>
-      
-      
-      
-      
-      
-
-    </ion-content>
-  </ion-page>
+  <section>
+    <img :src="picture" :width="size" :height="size" ref="imageEl" /><br />
+    <h1>ชื่อ-นามสกุล : {{ getFullName }}</h1>
+    <h1>อายุ : {{ age }} ปี</h1>
+    
+    <h1>อาชีพ : {{getDepartment}}</h1>
+    
+    <button @click="toggleVisible">{{ isVisible ? "ซ่อน" : "แสดง"}}รายละเอียด</button>
+    <article v-show="isVisible">
+      <p>ที่อยู่ : <span v-html="address"></span></p>
+      <p>Social : <a :href="social" target="_blank">Facebook</a></p>
+      <p v-if="hobby.length === 0">ไม่มีงานอดิเรก</p>
+      <div v-else>
+        <p>งานอดิเรก :</p>
+        <ul>
+          <li v-for="(item, index) in hobby" :key="index">{{ item }}</li>
+        </ul>
+      </div>
+      <p>ข้อมูลพื้นฐาน :</p>
+      <ul>
+        <li v-for="(item, key) in general" :key="key">{{ item }}</li>
+      </ul>
+    </article>
+  </section>
 </template>
 
-<script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonGrid, IonRow, IonCol } from '@ionic/vue';
-
-export default  {
-  name: 'Tab1',
-  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonButton, IonGrid, IonRow, IonCol }
-}
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      firstName: "Natdanai",
+      lastName: "Tinnakon",
+      age: 22,
+      address: "<i>พิจิตร</i>",
+      picture:"https://scontent.fphs2-1.fna.fbcdn.net/v/t1.6435-9/110320079_678019732751735_1561759245512041012_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=174925&_nc_ohc=eOn8pnYN7OEAX-pzauM&tn=G-cp_SvtvjVsKRCP&_nc_ht=scontent.fphs2-1.fna&oh=00_AT-BjzAgerOiEDbamU-rPbGnsyhpF2O3pfUkKGRHi5pCAg&oe=62380005",
+      size: 150,
+      social: "https://www.facebook.com/newz.natdanai.12/",
+      hobby: [
+        "ฟังเพลง",
+        "เล่นเกม",
+        "ดูทีวี",
+        "ท่องเน็ต",
+        "อ่านหนังสือ",
+        
+      ],
+      general: { gender: "ชาย", weight: 100, height: 170, status: false },
+      isVisible:false,
+      
+    };
+  },
+  methods: {
+    toggleVisible(){
+        this.isVisible = !this.isVisible
+    },
+    addSalary(value){
+        this.salary+=value
+    }
+  },
+  computed:{
+    getFullName() {
+        return `${this.firstName}  ${this.lastName}`;
+    },
+    getIncome(){
+        return this.salary * 12;
+    },
+    getDepartment(){
+        return this.salary >=35000 ? "Project manager" : "นักศึกษา"
+    }
+  },
+  
+  
+};
 </script>
+
+<style></style>
